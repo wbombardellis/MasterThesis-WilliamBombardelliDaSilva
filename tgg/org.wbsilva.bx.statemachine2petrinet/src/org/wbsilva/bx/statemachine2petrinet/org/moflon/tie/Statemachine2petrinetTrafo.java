9930d88689fe2a530ca8d4ace5b863597426c039
William Bombardelli da Silva <william.bombardellidasilva@campus.tu-berlin.de>
2018-05-28 18:16:02 +0200

    More two tgg transformations and metamodels added

---
+++ tgg/org.wbsilva.bx.statemachine2petrinet/src/org/wbsilva/bx/statemachine2petrinet/org/moflon/tie/Statemachine2petrinetTrafo.java
@@ -0,0 +1,83 @@
+package org.wbsilva.bx.statemachine2petrinet.org.moflon.tie;
+
+import java.io.IOException;
+import org.apache.log4j.BasicConfigurator;
+import org.moflon.tgg.algorithm.synchronization.SynchronizationHelper;
+
+import org.eclipse.emf.ecore.EObject;
+
+import org.wbsilva.bx.statemachine2petrinet.Statemachine2petrinetPackage;
+
+
+public class Statemachine2petrinetTrafo extends SynchronizationHelper{
+
+   public Statemachine2petrinetTrafo()
+   {
+      super(Statemachine2petrinetPackage.eINSTANCE, ".");
+   }
+
+	public static void main(String[] args) throws IOException {
+		// Set up logging
+        BasicConfigurator.configure();
+
+		// Forward Transformation
+        Statemachine2petrinetTrafo helper = new Statemachine2petrinetTrafo();
+		helper.performForward("instances/fwd.src.xmi");
+
+		// Backward Transformation
+		helper = new Statemachine2petrinetTrafo();
+		helper.performBackward("instances/bwd.src.xmi");
+	}
+
+	public void performForward() {
+		integrateForward();
+
+		saveTrg("instances/fwd.trg.xmi");
+		saveCorr("instances/fwd.corr.xmi");
+		saveSynchronizationProtocol("instances/fwd.protocol.xmi");
+
+		System.out.println("Completed forward transformation!");
+	}
+
+	public void performForward(EObject srcModel) {
+		setSrc(srcModel);
+		performForward();
+	}
+
+	public void performForward(String source) {
+		try {
+			loadSrc(source);
+			performForward();
+		} catch (IllegalArgumentException iae) {
+			System.err.println("Unable to load " + source + ", "
+					+ iae.getMessage());
+			return;
+		}
+	}
+
+	public void performBackward() {
+		integrateBackward();
+
+		saveSrc("instances/bwd.trg.xmi");
+		saveCorr("instances/bwd.corr.xmi");
+		saveSynchronizationProtocol("instances/bwd.protocol.xmi");
+
+		System.out.println("Completed backward transformation!");
+	}
+
+	public void performBackward(EObject targetModel) {
+		setTrg(targetModel);
+		performBackward();
+	}
+
+	public void performBackward(String target) {
+		try {
+			loadTrg(target);
+			performBackward();
+		} catch (IllegalArgumentException iae) {
+			System.err.println("Unable to load " + target + ", "
+					+ iae.getMessage());
+			return;
+		}
+	}
+}
