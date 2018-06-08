9996f0bde4ec8e914baead317be328bbc45ca2c8
William Bombardelli da Silva <william.bombardellidasilva@campus.tu-berlin.de>
2018-06-09 00:48:32 +0200

    Some architecture of unit tests added

---
+++ transformer/org.wbsilva.bence.transformer/test/org/wbsilva/bence/transformer/parser/BeNCEParserTest.java
@@ -0,0 +1,19 @@
+package org.wbsilva.bence.transformer.parser;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BeNCEParserTest {
+
+	@Test
+	void testBeNCEParser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testParse() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ transformer/org.wbsilva.bence.transformer/test/org/wbsilva/bence/transformer/parser/BupTest.java
@@ -0,0 +1,29 @@
+package org.wbsilva.bence.transformer.parser;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BupTest {
+
+	@Test
+	void testBup() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testNext() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAdd() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testHasNext() {
+		fail("Not yet implemented");
+	}
+
+}
