bccc727f732c22e6602d54c4e57827721d8cc60e
William Bombardelli da Silva <william.bombardellidasilva@campus.tu-berlin.de>
2018-06-10 19:06:11 +0200

    isomorphism check and further advances addded to the parser

---
+++ transformer/org.wbsilva.bence.graphgrammar/test/org/wbsilva/bence/graphgrammar/impl/EdgeImplTest.java
@@ -0,0 +1,14 @@
+package org.wbsilva.bence.graphgrammar.impl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class EdgeImplTest {
+
+	@Test
+	void testCompareTo() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ transformer/org.wbsilva.bence.graphgrammar/test/org/wbsilva/bence/graphgrammar/util/GraphgrammarUtilTest.java
@@ -0,0 +1,24 @@
+package org.wbsilva.bence.graphgrammar.util;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class GraphgrammarUtilTest {
+
+	@Test
+	void testAnyBijectiveMapping() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testIsomorphicEdges() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEnsureUniqueIds() {
+		fail("Not yet implemented");
+	}
+
+}
