48f456434578a898ad595dc3e6ee5c6d5d8eb38f
William Bombardelli da Silva <william.bombardellidasilva@campus.tu-berlin.de>
2018-07-18 19:46:17 +0200

    Ecore2GraphTransformer tests added and passed

--- transformer/org.wbsilva.bence.transformer/src/org/wbsilva/bence/transformer/ECore2GraphTransformer.java
+++ transformer/org.wbsilva.bence.transformer/src/org/wbsilva/bence/transformer/ECore2GraphTransformer.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EReference;
@@ -28,9 +29,12 @@ public class ECore2GraphTransformer {
 	static final Logger logger = LogManager.getLogger(ECore2GraphTransformer.class);
 	
 	/**
-	 * TODO
-	 * @param inputModel
-	 * @return
+	 * Transform the eCore/EMF model {@code inputModel} of an arbitrary metamodel to a graph 
+	 * of the Graphgrammar metamodel. Transform also recursively its children.
+	 * 
+	 * @param inputModel		Model to be transformed. Cannot be null.
+	 * @return					A graph resultant from the transformation from {@code inputModel} 
+	 * @see Graph
 	 */
 	public Graph transform(final EObject inputModel) {
 		assert inputModel != null;
@@ -48,96 +52,117 @@ public class ECore2GraphTransformer {
 	}
 
 	/**
-	 * TODO
-	 * @param graph
-	 * @param childObject
-	 * @return
+	 * Transform one eCore object {@code modelObject} into a vertex and its children recursively,
+	 * creating also edges if necessary and add them to the {@code graph}, reusing symbols and 
+	 * other already transformed vertices from {@code vertices} and {@code symbols}.
+	 * 
+	 * @param vertices		A map from the objects transformed so far to its correspondent vertices
+	 * @param symbols		A map from the objects' classes transformed so far to its correspondents labels
+	 * @param graph			The graph constructed so far, in which to add this vertex and its children as well as the edges 
+	 * @param modelObject	The eCore object to be transformed
+	 * @return				The vertex resultant from the transformation. Never null.
 	 */
 	private Vertex transformVertex(final ConcurrentHashMap<EObject, Vertex> vertices, final ConcurrentHashMap<EObject, Symbol> symbols, final Graph graph, final EObject modelObject) {
+		//EObject not transformed to a symbol yet
+		Vertex vertex = vertices.get(modelObject);
+		if (vertex == null) {		
+			vertex = GraphgrammarFactory.eINSTANCE.createVertex();
+			vertex.setId(EcoreUtil.generateUUID());
 		
-		final Vertex vertex = GraphgrammarFactory.eINSTANCE.createVertex();
-		vertex.setId(EcoreUtil.generateUUID());
-		
-		logger.debug(String.format("Transforing element %s to a vertex with id: %s", modelObject, vertex.getId()));
-		
-		final EClass eClass = modelObject.eClass();
-		Symbol label = symbols.get(eClass);
-		//EClass not transformed to a symbol yet
-		if (label == null) {
-			label = GraphgrammarFactory.eINSTANCE.createSymbol();
-			label.setName(eClass.getName());
+			logger.debug(String.format("Transforing element %s to a vertex with id: %s", modelObject, vertex.getId()));
 			
-			symbols.put(eClass, label);
-		}
-		
-		vertex.setLabel(EcoreUtil.copy(label));
-		graph.getVertices().add(vertex);
-		vertices.put(modelObject, vertex);
+			final EClass eClass = modelObject.eClass();
+			Symbol label = symbols.get(eClass);
+			//EClass not transformed to a symbol yet
+			if (label == null) {
+				label = GraphgrammarFactory.eINSTANCE.createSymbol();
+				label.setName(eClass.getName());
 				
-		//Processing of the contained children
-		final List<EReference> containments = eClass.getEAllContainments();
-		for (final EReference containment : containments) {
-			final Object childObject = modelObject.eGet(containment);
-			
-			if (childObject instanceof EObject) {
-				final Vertex childVertex = transformVertex(vertices, symbols, graph, (EObject) childObject);
-				assert childVertex != null;
-				
-				transformEdge(symbols, graph, vertex, childVertex, containment);
-				
-			} else {
-				logger.warn(String.format("Child object %s is not an EObject. Skipping it.", childObject));
+				symbols.put(eClass, label);
 			}
-		}
-		
-		//Processing of the other (not contained) children
-		final List<EReference> nonContainments = eClass.getEAllReferences().parallelStream()
-				.filter(r -> !r.isContainment())
-				.collect(Collectors.toList());
-		for (final EReference reference : nonContainments) {
-			final Object childObject = modelObject.eGet(reference);
 			
-			if (childObject instanceof EObject) {
-				Vertex childVertex = vertices.get((EObject) childObject);
-				//Vertex has not being transformed yet (e.g. model is not connected) 
-				if (childVertex == null) {
-					childVertex = transformVertex(vertices, symbols, graph, (EObject) childObject);					
+			vertex.setLabel(EcoreUtil.copy(label));
+			graph.getVertices().add(vertex);
+			vertices.put(modelObject, vertex);
+					
+			//Processing of the contained children
+			final List<EReference> containments = eClass.getEAllContainments();
+			for (final EReference containment : containments) {
+				final Object childObject = modelObject.eGet(containment);
+				
+				if (childObject instanceof EObject) {
+					final Vertex childVertex = transformVertex(vertices, symbols, graph, (EObject) childObject);
+					assert childVertex != null;
+					
+					transformEdge(symbols, graph, vertex, childVertex, containment);
+					
+				} else if (childObject instanceof EList<?>){
+					@SuppressWarnings("unchecked")
+					EList<EObject> children = (EList<EObject>)childObject;
+					for (EObject child : children) {
+						final Vertex childVertex = transformVertex(vertices, symbols, graph, child);
+						assert childVertex != null;
+						
+						transformEdge(symbols, graph, vertex, childVertex, containment);
+					}
+				} else {
+					logger.warn(String.format("Child object %s is not an EObject. Skipping it.", childObject));
 				}
-				assert childVertex != null;
-				
-				transformEdge(symbols, graph, vertex, childVertex, reference);
-				
-			} else {
-				logger.debug(String.format("Child object %s is not an EObject. Skipping it.", childObject));
 			}
+			
+			//Processing of the other (not contained) children
+			final List<EReference> nonContainments = eClass.getEAllReferences().parallelStream()
+					.filter(r -> !r.isContainment())
+					.collect(Collectors.toList());
+			for (final EReference reference : nonContainments) {
+				final Object childObject = modelObject.eGet(reference);
+				
+				if (childObject instanceof EObject) {
+					Vertex childVertex = vertices.get((EObject) childObject);
+					//Vertex has not being transformed yet (e.g. model is not connected) 
+					if (childVertex == null) {
+						childVertex = transformVertex(vertices, symbols, graph, (EObject) childObject);					
+					}
+					assert childVertex != null;
+					
+					transformEdge(symbols, graph, vertex, childVertex, reference);
+					
+				} else {
+					logger.debug(String.format("Child object %s is not an EObject. Skipping it.", childObject));
+				}
+			}
+		} else {
+			logger.debug(String.format("Element %s has already been transformed to the vertex with id: %s. Using it.", modelObject, vertex.getId()));
 		}
 		
 		return vertex;
 	}
 
 	/**
-	 * TODO
-	 * @param symbols
-	 * @param graph
-	 * @param from
-	 * @param to
-	 * @param toOriginalObject
-	 * @return
+	 * Create a directed edge from {@code from} to {@code to} and add it to {@code graph} reusing symbols from
+	 * {@code symbols}
+	 * 
+	 * @param symbols			A map from the objects' classes transformed so far to its correspondents labels
+	 * @param graph				The graph constructed so far, in which to add this edge
+	 * @param from				The origin vertex
+	 * @param to				The target vertex
+	 * @param reference			The attribute of the source object of {@code from} holding the source object {@code to}
+	 * @return					The new edge. Never null.
 	 */
-	private Edge transformEdge(final ConcurrentHashMap<EObject, Symbol> symbols, final Graph graph, final Vertex from, final Vertex to, final EReference toOriginalObject) {
+	private Edge transformEdge(final ConcurrentHashMap<EObject, Symbol> symbols, final Graph graph, final Vertex from, final Vertex to, final EReference reference) {
 		logger.debug(String.format("Transforing edge from %s to %s", from.getId(), to.getId()));
 		
 		final Edge edge = GraphgrammarFactory.eINSTANCE.createEdge();
 		edge.setFrom(from);
 		edge.setTo(to);
 		
-		Symbol edgeLabel = symbols.get(toOriginalObject);
+		Symbol edgeLabel = symbols.get(reference);
 		//EClass not transformed to a symbol yet
 		if (edgeLabel == null) {
 			edgeLabel = GraphgrammarFactory.eINSTANCE.createSymbol();
-			edgeLabel.setName(toOriginalObject.getName());
+			edgeLabel.setName(reference.getName());
 			
-			symbols.put(toOriginalObject, edgeLabel);
+			symbols.put(reference, edgeLabel);
 		}
 		
 		edge.setLabel(EcoreUtil.copy(edgeLabel));
--- transformer/org.wbsilva.bence.transformer/test/org/wbsilva/bence/transformer/ECore2GraphTransformerTest.java
+++ transformer/org.wbsilva.bence.transformer/test/org/wbsilva/bence/transformer/ECore2GraphTransformerTest.java
@@ -2,13 +2,119 @@ package org.wbsilva.bence.transformer;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.EReference;
+import org.eclipse.emf.ecore.EcoreFactory;
+import org.eclipse.emf.ecore.EcorePackage;
+import org.eclipse.emf.ecore.impl.EObjectImpl;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.wbsilva.bence.graphgrammar.Graph;
+import org.wbsilva.bence.graphgrammar.util.GraphgrammarUtil;
 
 class ECore2GraphTransformerTest {
 
+	private static EObject obj1;
+	private static EObject obj2;
+	private static EObject obj3;
+	private static EObject obj4;
+
+	@SuppressWarnings("unchecked")
+	@BeforeAll
+	static void init() {
+		EPackage pkg = EcoreFactory.eINSTANCE.createEPackage();
+		pkg.setName("PKG");
+		pkg.setNsPrefix("org.wbsilva.mm.test");
+		
+		EClass clazzA = EcoreFactory.eINSTANCE.createEClass();
+		clazzA.setName("A");
+		
+		pkg.getEClassifiers().add(clazzA);
+		
+		EReference refB = EcoreFactory.eINSTANCE.createEReference();
+		refB.setName("B");
+		refB.setContainment(true);
+		refB.setChangeable(true);
+		refB.setEType(clazzA);
+		refB.setUpperBound(-1);
+		
+		EReference refC = EcoreFactory.eINSTANCE.createEReference();
+		refC.setName("C");
+		refC.setContainment(false);
+		refC.setChangeable(true);
+		refC.setEType(clazzA);
+		refC.setUpperBound(1);
+		
+		clazzA.getEStructuralFeatures().add(refB);
+		clazzA.getEStructuralFeatures().add(refC);
+		
+		obj1 = pkg.getEFactoryInstance().create(clazzA);
+		
+		obj2 = pkg.getEFactoryInstance().create(clazzA);
+		obj2.eSet(refC, obj1);
+		
+		obj3 = pkg.getEFactoryInstance().create(clazzA);
+		((List<EObject>)obj3.eGet(refB)).add(obj1);
+		obj3.eSet(refC, obj2);
+		
+		obj4 = pkg.getEFactoryInstance().create(clazzA);
+		((List<EObject>)obj4.eGet(refB)).add(obj3);
+		((List<EObject>)obj4.eGet(refB)).add(obj4);
+	}
+	
+	
 	@Test
-	void testTransform() {
-		fail("Not yet implemented");
+	void testTransformOne() {
+		ECore2GraphTransformer transformer = new ECore2GraphTransformer();
+		Graph g0 = transformer.transform(obj1);
+		
+		assertTrue(GraphgrammarUtil.isValidGraph(g0));
+		assertEquals(1, g0.getVertices().size());
+		assertEquals("A", g0.getVertices().get(0).getLabel().getName());
 	}
 
+	@Test
+	void testTransformWithOneChild() {
+		ECore2GraphTransformer transformer = new ECore2GraphTransformer();
+		Graph g0 = transformer.transform(obj2);
+		
+		assertTrue(GraphgrammarUtil.isValidGraph(g0));
+		assertEquals(2, g0.getVertices().size());
+		assertTrue(g0.getVertices().parallelStream().allMatch(v -> v.getLabel().getName().equals("A")));
+		
+		assertEquals(1, g0.getEdges().size());
+		assertEquals("C", g0.getEdges().get(0).getLabel().getName());
+	}
+	
+	@Test
+	void testTransformWithNonContainedChildAndLists() {
+		ECore2GraphTransformer transformer = new ECore2GraphTransformer();
+		Graph g0 = transformer.transform(obj3);
+		
+		assertTrue(GraphgrammarUtil.isValidGraph(g0));
+		assertEquals(3, g0.getVertices().size());
+		assertTrue(g0.getVertices().parallelStream().allMatch(v -> v.getLabel().getName().equals("A")));
+		
+		assertEquals(3, g0.getEdges().size());
+		assertTrue(g0.getEdges().parallelStream()
+				.allMatch(e -> e.getLabel().getName().equals("B") || e.getLabel().getName().equals("C")));
+	}
+	
+	@Test
+	void testTransformLoop() {
+		ECore2GraphTransformer transformer = new ECore2GraphTransformer();
+		Graph g0 = transformer.transform(obj4);
+		
+		assertTrue(GraphgrammarUtil.isValidGraph(g0));
+		assertEquals(4, g0.getVertices().size());
+		assertTrue(g0.getVertices().parallelStream().allMatch(v -> v.getLabel().getName().equals("A")));
+		
+		assertEquals(5, g0.getEdges().size());
+		assertTrue(g0.getEdges().parallelStream()
+				.allMatch(e -> e.getLabel().getName().equals("B") || e.getLabel().getName().equals("C")));
+	}
 }
