ff341df99a5a3d06b02983d290298df339b1b498
William Bombardelli da Silva <william.bombardellidasilva@campus.tu-berlin.de>
2018-07-23 12:34:16 +0200

    fix isomorphism helpers and fix/add checks

--- transformer/org.wbsilva.bence.graphgrammar/src-gen/org/wbsilva/bence/graphgrammar/util/GraphgrammarUtil.java
+++ transformer/org.wbsilva.bence.graphgrammar/src-gen/org/wbsilva/bence/graphgrammar/util/GraphgrammarUtil.java
@@ -2,22 +2,26 @@ package org.wbsilva.bence.graphgrammar.util;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
-import java.util.function.BiFunction;
-import java.util.function.Function;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
+import org.eclipse.emf.common.util.BasicEList;
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.common.util.EMap;
 import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.wbsilva.bence.graphgrammar.Derivation;
+import org.wbsilva.bence.graphgrammar.DerivationStep;
 import org.wbsilva.bence.graphgrammar.Edge;
 import org.wbsilva.bence.graphgrammar.Grammar;
 import org.wbsilva.bence.graphgrammar.Graph;
 import org.wbsilva.bence.graphgrammar.GraphgrammarFactory;
 import org.wbsilva.bence.graphgrammar.Rule;
 import org.wbsilva.bence.graphgrammar.Symbol;
+import org.wbsilva.bence.graphgrammar.SymbolSymbolsPair;
 import org.wbsilva.bence.graphgrammar.TripleGrammar;
 import org.wbsilva.bence.graphgrammar.TripleGraph;
 import org.wbsilva.bence.graphgrammar.TripleRule;
@@ -31,26 +35,27 @@ import org.wbsilva.bence.graphgrammar.Vertex;
 public class GraphgrammarUtil {
 
 	/**
-	 * Return true iff a {@code mapping} represents part of a bijective mapping constructed from the list of candidates 
-	 * for each vertex {@code candidates}.
+	 * Iff a {@code mapping} represents part of a bijective mapping constructed from the list of candidates 
+	 * for each vertex {@code candidates}, then return {@code mapping} in an optional. Otherwise empty
 	 * Each element i of this list holds the set of candidate vertices for the mapping's image of the i-th vertex
 	 * and each element i of the list {@code mapping} holds the chosen image for the i-th vertex.
 	 * This method supposes that the union of all candidate vertices belongs to the n vertices of the mapping's domain.
 	 * 
 	 * @param mapping			The partial mapping constructed so far
 	 * @param candidates		The set of candidate vertices for image of each of the n vertices 
-	 * @return					True if mapping is part of a bijection from the n vertices (each one represented 
-	 * 							by each position of the candidates list) to the candidate vertices, false otherwise.
+	 * @return					If {@code mapping} is part of a bijection from the n vertices (each one represented 
+	 * 							by each position of the candidates list) to the candidate vertices, 
+	 * 							then return an optional with mapping inside, empty otherwise.
 	 */
-	private static boolean anyBijectiveMapping(final List<Vertex> mapping, final List<Set<Vertex>> candidates) {
+	private static Optional<List<Vertex>> anyBijectiveMapping(final List<Vertex> mapping, final List<Set<Vertex>> candidates) {
 		//if mapping is complete (it got to a leaf of the search space)
 		if (mapping.size() == candidates.size()) {
 			//if mapping is bijective
 			if (mapping.stream().distinct().count() == mapping.size()) {
 				//then it is an isomorphism
-				return true;
+				return Optional.of(mapping);
 			} else {
-				return false;
+				return Optional.empty();
 			}
 		}
 		//if mapping still not ready
@@ -68,24 +73,27 @@ public class GraphgrammarUtil {
 			for (Vertex c : candidates.get(index)) {
 				newMapping.set(index, c);
 				//if this mapping works, return true, otherwise keep trying
-				if (anyBijectiveMapping(newMapping, candidates))
-					return true;				
+				final Optional<List<Vertex>> m = anyBijectiveMapping(newMapping, candidates);
+				if (m.isPresent())
+					return m;				
 			}
 			//all possibilities exhausted
-			return false;
+			return Optional.empty();
 		}
 	}
 	
 	/**
-	 * Return true iff a bijective mapping can be constructed from the list of candidates for each vertex {@code candidates}.
+	 * If a bijective mapping can be constructed from the list of candidates for each vertex {@code candidates},
+	 * then return it, otherwise return empty.
 	 * Each element i of this list holds the set of candidate vertices for the mapping's image of the i-th vertex.
 	 * This method supposes that the union of all candidate vertices belongs to the n vertices of the mapping's domain.
 	 * 
 	 * @param candidates		The set of candidate vertices for image of each of the n vertices 
-	 * @return					True if there is a bijection from the n vertices (each one represented 
-	 * 							by each position of the candidates list) to the candidate vertices, false otherwise. 
+	 * @return					A bijection from the n vertices (each one represented 
+	 * 							by each position of the candidates list) to the candidate vertices, if it is an isomorphism,
+	 * 							empty if none exists. 
 	 */
-	public static boolean anyBijectiveMapping(final List<Set<Vertex>> candidates) {
+	public static Optional<List<Vertex>> anyBijectiveMapping(final List<Set<Vertex>> candidates) {
 		assert candidates != null;
 		return anyBijectiveMapping(new ArrayList<Vertex>(0), candidates);
 	}
@@ -270,7 +278,11 @@ public class GraphgrammarUtil {
 		if (graph.getVertices().parallelStream().map(v -> v.getId()).distinct().count() != graph.getVertices().size())
 			return false;
 		
-		if (graph.getEdges().parallelStream().anyMatch(e -> !graph.getVertices().contains(e.getFrom()) || !graph.getVertices().contains(e.getTo())))
+		if (graph.getEdges().parallelStream()
+				.anyMatch(e -> !graph.getVertices().contains(e.getFrom()) 
+							|| !graph.getVertices().contains(e.getTo())
+							|| e.getLabel() == null || e.getLabel().getName().isEmpty()
+							|| e.getFrom() == e.getTo()))
 			return false;
 		
 		return true;
@@ -311,7 +323,7 @@ public class GraphgrammarUtil {
 		if (r.parallelStream().anyMatch(rr -> !isValidRule(ab, rr.getSource()) || !isValidRule(ab, rr.getCorr()) || !isValidRule(ab, rr.getTarget())))
 				return false;
 		
-		if (r.parallelStream().anyMatch(rr -> !isTotal(rr.getCorr().getRhs().getVertices(), rr.getMs()) ||
+		if (r.stream().anyMatch(rr -> !isTotal(rr.getCorr().getRhs().getVertices(), rr.getMs()) ||
 				!isInjective(rr.getMs()) ||
 				!isSurjective(rr.getSource().getRhs().getVertices(), rr.getMs()) || 
 				!isTotal(rr.getCorr().getRhs().getVertices(), rr.getMt()) ||
@@ -328,7 +340,7 @@ public class GraphgrammarUtil {
 	 * @return					True iff {@code map} is surjective
 	 */
 	private static boolean isSurjective(final EList<Vertex> counterDomain, final EMap<Vertex, Vertex> map) {
-		return counterDomain.parallelStream().anyMatch(cd -> !map.containsValue(cd)) ? false : true;
+		return counterDomain.stream().anyMatch(cd -> !map.containsValue(cd)) ? false : true;
 	}
 
 	/**
@@ -338,7 +350,7 @@ public class GraphgrammarUtil {
 	 * @return					True iff {@code map} is total
 	 */
 	private static boolean isTotal(final EList<Vertex> domain, final EMap<Vertex, Vertex> map) {
-		return domain.parallelStream().anyMatch(d -> !map.containsKey(d)) ? false : true;
+		return domain.stream().anyMatch(d -> !map.containsKey(d)) ? false : true;
 	}
 
 	/**
@@ -347,7 +359,7 @@ public class GraphgrammarUtil {
 	 * @return					True iff {@code map} is injective
 	 */
 	private static boolean isInjective(final EMap<Vertex, Vertex> map) {
-		return map.values().parallelStream().distinct().count() != map.size() ? false : true;
+		return map.values().stream().distinct().count() != map.size() ? false : true;
 	}
 
 	/**
@@ -375,14 +387,23 @@ public class GraphgrammarUtil {
 
 	/**
 	 * Checks if a grammar is a boundary grammar
-	 * @param grammar		The grammar to check
+	 * @param grammar		The grammar to check. Has to be valid
 	 * @return				True iff {@code grammar} is boundary
 	 */
 	public static boolean isBoundaryGrammar(final Grammar grammar) {
+		final Set<String> terminals = grammar.getTerminals().parallelStream()
+				.map(l -> l.getName())
+				.collect(Collectors.toSet());
 		
 		//if any rule's graph is not boundary 
 		if (grammar.getRules().parallelStream()
-			.anyMatch(r -> !isBoundaryGraph(r.getRhs(), grammar.getNonterminals())))
+			.anyMatch(r -> !isBoundaryGraph(r.getRhs(), grammar.getNonterminals())					
+					//Check range of the embedding function
+					|| !r.getEmbedding().values().parallelStream()
+							.allMatch(e -> e.parallelStream()
+									.allMatch(ssP -> ssP
+											.getVertexLabels().parallelStream()
+												.allMatch(l -> terminals.contains(l.getName()))))))
 			return false;
 		
 		return true;
@@ -390,7 +411,7 @@ public class GraphgrammarUtil {
 
 	/**
 	 * Checks if a graph is a G-boundary graph
-	 * @param grammar		The graph to check
+	 * @param grammar		The graph to check. Has to be valid
 	 * @param g				The G set used to check the boundaryness
 	 * @return				True iff {@code graph} is {@code g}-boundary
 	 */
@@ -398,9 +419,7 @@ public class GraphgrammarUtil {
 		//if the graph has G-labeled vertices as neighbors
 		if(graph.getEdges().parallelStream()
 				.anyMatch(e -> inAlphabet(g, e.getFrom().getLabel()) &&
-						inAlphabet(g, e.getTo().getLabel()))
-				//or it is not (weakly) connected
-				|| !isWeaklyConnectedGraph(graph))
+						inAlphabet(g, e.getTo().getLabel())))
 			//then it is not G-boundary
 			return false;
 		
@@ -409,17 +428,17 @@ public class GraphgrammarUtil {
 	
 	/**
 	 * Checks if a directed graph is weakly connected
-	 * @param grammar		The graph to check
+	 * @param grammar		The graph to check. Has to be valid
 	 * @return				True iff {@code graph} is weakly connected
 	 */
-	private static boolean isWeaklyConnectedGraph(final Graph graph) {
+	public static boolean isWeaklyConnectedGraph(final Graph graph) {
 		//TODO
 		return true;
 	}
 
 	/**
 	 * Checks if a triple grammar is a boundary triple grammar
-	 * @param tripleGrammar		The triple grammar to check
+	 * @param tripleGrammar		The triple grammar to check. Has to be valid
 	 * @return					True iff {@code tripleGrammar} is boundary
 	 */
 	public static boolean isBoundaryTripleGrammar(final TripleGrammar tripleGrammar) {
@@ -430,6 +449,127 @@ public class GraphgrammarUtil {
 					|| !isBoundaryGraph(tr.getTarget().getRhs(), tripleGrammar.getNonterminals())))
 			return false;
 		
+		return true;
+	}
+
+	/**
+	 * Checks a sufficient (but not necessary) condition for a grammar to be neighborhood preserving.
+	 * Does not say anything about it not being neighborhood preserving.
+	 * 
+	 * @param grammar		The grammar to check. Has to be valid
+	 * @return				If True, then {@code grammar} is neighborhood preserving.
+	 * 						If False, then {@code grammar} may or may not be neighborhood preserving.
+	 */
+	public static boolean isNeighborhoodPreserving(final Grammar grammar) {
+		final HashMap<String, Set<String>> maxContext = new HashMap<>(grammar.getNonterminals().size());
+		final HashMap<Vertex, Set<String>> embeddingContext = new HashMap<>();
+
+		for (Symbol l: grammar.getNonterminals()) {
+			maxContext.put(l.getName(), new HashSet<String>());
+		}
+		
+		//Determine the maximal context of each nonterminal symbol
+		for (Rule r : grammar.getRules()) {
+			for (Vertex v : r.getRhs().getVertices()) {
+				
+				//The embedding context of each vertex of the RHS of each rule
+				final EList<SymbolSymbolsPair> embedding = r.getEmbedding().get(v);
+				final Set<String> vertexEmbeddingContext;
+				if (embedding == null) {
+					vertexEmbeddingContext = new HashSet<>(0);
+				} else {
+					vertexEmbeddingContext = embedding.parallelStream()
+						.flatMap(ssp -> ssp.getVertexLabels().parallelStream()
+								.map(s -> s.getName()))
+						.collect(Collectors.toSet());
+				}
+				embeddingContext.put(v, vertexEmbeddingContext);
+				
+				if (grammar.getNonterminals().parallelStream().anyMatch(l -> EcoreUtil.equals(l, v.getLabel()))) {
+					final Set<String> ntContext = maxContext.get(v.getLabel().getName());
+					
+					//The real context of each nonterminal vertex of the RHS of each rule
+					ntContext.addAll(r.getRhs().neighborhood(v).parallelStream()
+							.map(w -> w.getLabel().getName())
+							.collect(Collectors.toSet()));
+					
+					ntContext.addAll(vertexEmbeddingContext);
+				}
+			}
+		}
+		
+		//If any rule's embedding context does not contain all LHS's maximal context
+		if (grammar.getRules().parallelStream()
+				.anyMatch(r -> !r.getRhs().getVertices().parallelStream()
+						.flatMap(v -> embeddingContext.get(v).stream())
+						.collect(Collectors.toSet())
+						.containsAll(maxContext.get(r.getLhs().getName()))))
+			//Then it may not be a neighborhood preserving grammar
+			return false;
+		else
+			return true;
+	}
+
+	/**
+	 * Checks if {@code ds} is neighborhood preserving
+	 * @param ds		The derivation step to be checked. Has to be valid
+	 * @return			True iff {@code ds} is neighborhood preserving.
+	 */
+	public static boolean isNeighborhoodPreserving(final DerivationStep ds) {
+		final Vertex vertex = ds.getPrevious().getVertices().parallelStream().filter(w -> w.getId().equals(ds.getVertex().getId())).findAny().orElse(null);
+		assert vertex != null;
+		
+		//iff neighbors of vertex are equal to neighbors of the RHS neigh_prev(vertex) = neigh_next(V_Y)
+		final EList<Vertex> n1 = ds.getPrevious().neighborhood(vertex);
+		final EList<Vertex> n2 = ds.getNext().neighborhood(new BasicEList<>(ds.getRule().getRhs().getVertices().stream()
+				.map(v -> ds.getUnifier().get(v))
+				.collect(Collectors.toSet())));
+		
+		final Graph g1 = GraphgrammarFactory.eINSTANCE.createGraph();
+		g1.getVertices().addAll(EcoreUtil.copyAll(n1));
+		final Graph g2 = GraphgrammarFactory.eINSTANCE.createGraph();
+		g2.getVertices().addAll(EcoreUtil.copyAll(n2));
+		
+		return g1.isomorphicTo(g2);
+	}
+
+	/**
+	 * Checks if a derivation is valid or not (It does not check each derivation step fully validity)
+	 * @param derivation		The derivation to check
+	 * @return
+	 */
+	public static boolean isValidDerivation(final Derivation derivation) {
+		if (derivation == null)
+			return false;
+		if (derivation.getSteps().isEmpty())
+			return false;
+		
+		if (derivation.getSteps().stream().anyMatch(dS -> !GraphgrammarUtil.isValidDerivationStep(dS)))
+			return false;
+		
+		return true;
+	}
+
+	/**
+	 * Checks if a derivation step is valid or not
+	 * 
+	 * @param dS		The derivation step to check
+	 * @return			True iff {@code dS} is valid
+	 */
+	public static boolean isValidDerivationStep(final DerivationStep dS) {
+		if (!GraphgrammarUtil.isValidGraph(dS.getPrevious()))
+			return false;
+		if (!GraphgrammarUtil.isValidGraph(dS.getNext()))
+			return false;
+		if (!GraphgrammarUtil.isValidRule(dS.getRule()))
+			return false;
+		if (dS.getVertex() == null || !dS.getPrevious().getVertices().parallelStream().anyMatch(v -> EcoreUtil.equals(v, dS.getVertex())))
+			return false;
+		if (dS.getUnifier() == null || dS.getUnifier().size() != dS.getRule().getRhs().getVertices().size()
+				|| !isInjective(dS.getUnifier())
+				|| !isTotal(dS.getRule().getRhs().getVertices(), dS.getUnifier())
+				|| !dS.getUnifier().parallelStream().allMatch(u -> dS.getNext().getVertices().contains(u.getValue())))
+			return false;
 		return true;
 	}
 
