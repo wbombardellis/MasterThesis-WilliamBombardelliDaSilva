5d001c70e285404d00c8a02a279e415c9dca0819
William Bombardelli da Silva <william.bombardellidasilva@campus.tu-berlin.de>
2018-07-25 20:15:52 +0200

    adapt transformer to use NP normalizer

--- transformer/org.wbsilva.bence.transformer/src/org/wbsilva/bence/transformer/BeNCETransformer.java
+++ transformer/org.wbsilva.bence.transformer/src/org/wbsilva/bence/transformer/BeNCETransformer.java
@@ -107,61 +107,59 @@ public class BeNCETransformer {
 				}
 				logger.debug("Target grammar generated");
 			}
+			
+			//Make grammar neighborhood preserving
+			new NPNormalizer().normalize(inputGrammar);
+			
 			assert GraphgrammarUtil.isValidGrammar(inputGrammar);
 			assert GraphgrammarUtil.isBoundaryGrammar(inputGrammar);
+			assert GraphgrammarUtil.isNeighborhoodPreserving(inputGrammar);
 			
-			//Check neighborhood preserving property
-			if (GraphgrammarUtil.isNeighborhoodPreserving(inputGrammar)) {
+			//Parse input graph
+			final BeNCEParser parser = new BeNCEParser(inputGrammar);
+			final Optional<ParsingTree> parsingTree = parser.parse(inputGraph);
+			assert parsingTree != null;
 			
-				//Parse input graph
-				final BeNCEParser parser = new BeNCEParser(inputGrammar);
-				final Optional<ParsingTree> parsingTree = parser.parse(inputGraph);
-				assert parsingTree != null;
+			//Construct output graph in form of a triple graph
+			if (parsingTree.isPresent()) {
+				final Derivation derivation = parsingTree.get().derivation();
+				assert GraphgrammarUtil.isValidDerivation(derivation);
 				
-				//Construct output graph in form of a triple graph
-				if (parsingTree.isPresent()) {
-					final Derivation derivation = parsingTree.get().derivation();
-					assert GraphgrammarUtil.isValidDerivation(derivation);
+				logger.debug("Starting final triple graph assembly");
+				
+				//Set initial state of tripleGraph with initial symbols
+				final TripleGraph tripleGraph = createInitialTripleGraph(derivation, forward);
+				assert tripleGraph != null;
+				assert GraphgrammarUtil.isValidTripleGraph(tripleGraph);
+				
+				//Produce for each rule used in the parsing the respective target (or source) part of the triple graph
+				for (DerivationStep dStep : derivation.getSteps()) {
+					assert dStep != null;
 					
-					logger.debug("Starting final triple graph assembly");
+					final int oldTargetSize = forward ? tripleGraph.getTarget().getVertices().size()
+							: tripleGraph.getSource().getVertices().size();
 					
-					//Set initial state of tripleGraph with initial symbols
-					final TripleGraph tripleGraph = createInitialTripleGraph(derivation, forward);
-					assert tripleGraph != null;
-					assert GraphgrammarUtil.isValidTripleGraph(tripleGraph);
+					tripleGrammar.produce(tripleGraph, dStep, forward);
 					
-					//Produce for each rule used in the parsing the respective target (or source) part of the triple graph
-					for (DerivationStep dStep : derivation.getSteps()) {
-						assert dStep != null;
-						
-						final int oldTargetSize = forward ? tripleGraph.getTarget().getVertices().size()
-								: tripleGraph.getSource().getVertices().size();
-						
-						tripleGrammar.produce(tripleGraph, dStep, forward);
-						
-						assert forward ? tripleGraph.getTarget().getVertices().size() >= oldTargetSize 
-								: tripleGraph.getSource().getVertices().size()  >= oldTargetSize;
-						
-						if (forward)
-							logger.debug(String.format("Rule [id = %s, name = %s] applied to the triple graph. Target graph size = %s", dStep.getRule().getId(), dStep.getRule().getName(), tripleGraph.getTarget().getVertices().size()));
-						else
-							logger.debug(String.format("Rule [id = %s, name = %s] applied to the triple graph. Source graph size = %s", dStep.getRule().getId(), dStep.getRule().getName(), tripleGraph.getSource().getVertices().size()));
-			
-					}
+					assert forward ? tripleGraph.getTarget().getVertices().size() >= oldTargetSize 
+							: tripleGraph.getSource().getVertices().size()  >= oldTargetSize;
 					
-					logger.debug("Final triple grammar assembly finished");
-					logger.debug("Transformation finished successfully");
-					return Optional.of(new TransformationResult(tripleGraph, parsingTree.get()));
-				} else {
-					logger.debug("Could not parse input graph using the input grammar");
-					logger.debug("Transformation finished without success");
-					return Optional.empty();
+					if (forward)
+						logger.debug(String.format("Rule [id = %s, name = %s] applied to the triple graph. Target graph size = %s", dStep.getRule().getId(), dStep.getRule().getName(), tripleGraph.getTarget().getVertices().size()));
+					else
+						logger.debug(String.format("Rule [id = %s, name = %s] applied to the triple graph. Source graph size = %s", dStep.getRule().getId(), dStep.getRule().getName(), tripleGraph.getSource().getVertices().size()));
+		
 				}
+				
+				logger.debug("Final triple grammar assembly finished");
+				logger.debug("Transformation finished successfully");
+				return Optional.of(new TransformationResult(tripleGraph, parsingTree.get()));
 			} else {
-				logger.warn(String.format("Input grammar %s may not be neighborhood preserving. Aborting", inputGrammar.getName()));
-				logger.warn("Transformation finished without success");
+				logger.debug("Could not parse input graph using the input grammar");
+				logger.debug("Transformation finished without success");
 				return Optional.empty();
 			}
+			
 		}
 	}
 
