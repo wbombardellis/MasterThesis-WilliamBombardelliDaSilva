b2e291a67ba93251143a0799f93228962318ebc7
wbo <wbo@10.147.66.42>
2018-08-31 14:19:13 +0200

    Remove second criterion of GreedyBup and add support for extensions
    -canEnqueue and ssComparator extensions

--- transformer/org.wbsilva.bence.transformer/src/org/wbsilva/bence/transformer/parser/GreedyBup.java
+++ transformer/org.wbsilva.bence.transformer/src/org/wbsilva/bence/transformer/parser/GreedyBup.java
@@ -1,18 +1,17 @@
 package org.wbsilva.bence.transformer.parser;
 
 import java.util.Comparator;
+import java.util.HashSet;
 import java.util.Optional;
 import java.util.PriorityQueue;
 import java.util.Set;
 
-import org.wbsilva.bence.graphgrammar.Graph;
 import org.wbsilva.bence.graphgrammar.ZoneVertex;
 
 /**
  * This extension of {@linkplain Bup} implements a greedy strategy that creates subsets of different sizes
  * upon the addition of a new zone vertex and retrieves them ordered by the amount of vertices inside it. 
- * That is, sets with zone vertices containing more vertices are retrieved first. If they have the same amount
- * of vertices, furhter criteria are used to order them.
+ * That is, sets with zone vertices containing more vertices are retrieved first
  * 
  * @author wbombardellis
  *
@@ -28,20 +27,19 @@ class GreedyBup extends Bup {
 	final protected PriorityQueue<Set<ZoneVertex>> centralQueue;
 	
 	/**
-	 * The graph to which the vertices in the zone vertices belong. Used for heuristics only.
-	 */
-	final protected Graph graph;
-	
-	/**
 	 * The comparator for the sets of zone vertices. The more vertices the zone vertices of a set has,
 	 * the smaller it is in this total order. If two sets have the same amount of vertices in their
-	 * zone vertices, then further criteria decide their order.
+	 * zone vertices, then they are equal.
 	 * This order is not consistent with equals
 	 */
-	final private Comparator<Set<ZoneVertex>> ZVSET_COMPARATOR = new Comparator<Set<ZoneVertex>>() {
+	static final private Comparator<Set<ZoneVertex>> ZVSET_COMPARATOR = new Comparator<Set<ZoneVertex>>() {
 
 		/**
 		 * This method imposes a total order on the sets of zone vertices.
+		 * Semi formally the order is defined as follows
+		 * a < b iff a.zoneVertices.vertices.size > b.zoneVertices.vertices.size
+		 * a = b iff a.zoneVertices.vertices.size = b.zoneVertices.vertices.size
+		 * a > b iff a.zoneVertices.vertices.size < b.zoneVertices.vertices.size
 		 */
 		@Override
 		public int compare(final Set<ZoneVertex> a, final Set<ZoneVertex> b) {
@@ -58,46 +56,16 @@ class GreedyBup extends Bup {
 					.orElse(0);
 			
 			//A bigger set a is smaller in our order, so that it goes in the beginning of the queue
-			if (aSize > bSize) {
-				return -1;
-			} else if (aSize < bSize) {
-				return 1;
-			} else {
-				//The second criterion is the amount of outEdges
-				final int aOutEdges = a.stream()
-								.mapToInt(z -> z.getVertices().stream()
-													.mapToInt(v -> graph.outEdges(v).size())
-													.reduce(Integer::sum)
-													.orElse(0))
-								.reduce(Integer::sum)
-								.orElse(0);
-				final int bOutEdges = b.stream()
-						.mapToInt(z -> z.getVertices().stream()
-											.mapToInt(v -> graph.outEdges(v).size())
-											.reduce(Integer::sum)
-											.orElse(0))
-						.reduce(Integer::sum)
-						.orElse(0);
-				if (aOutEdges < bOutEdges) {
-					return -1;
-				} else if (aOutEdges > bOutEdges) {
-					return 1;					
-				} else {
-					return 0;
-				}
-			}
+			return aSize > bSize ? -1 : aSize == bSize ? 0 : 1;
 		}
 	};
 	
 	/**
-	 * Same contract as {@link Bup#Bup(Set, int, ZoneVertex)} with additional parameter for the graph.
-	 * @param graph			The graph to which the vertices of the zone vertices belong
+	 * Same contract as {@link Bup#Bup(Set, int, ZoneVertex)}
 	 * @see Bup
 	 */
-	public GreedyBup(final Set<ZoneVertex> initialSet, final int maximalSubsetSize, final ZoneVertex root, final Graph graph) {
+	public GreedyBup(final Set<ZoneVertex> initialSet, final int maximalSubsetSize, final ZoneVertex root) {
 		super(initialSet, maximalSubsetSize, root);
-		
-		this.graph = graph;
 		
 		//The central queue holds all generated but not yet consumed subsets ordered in their size
 		this.centralQueue = new PriorityQueue<>(ZVSET_COMPARATOR);
@@ -110,15 +78,30 @@ class GreedyBup extends Bup {
 	}
 	
 	/**
-	 * Add new subsets to the {@code subsets}  at the end of it, that is, at the position corresponding to a phase.
-	 * as well as to the {@code centralQueue} according to the priority criteria.
-	 * 
-	 * @param newSubsets	The set of subsets of zone vertices to be added to the end of the {@code subsets} and {@code centralQueue}
+	 * Same contract as {@link Bup#Bup(Set, int, ZoneVertex)} with additional parameter for the subsets comparator
+	 * @see Bup
+	 * @see Comparator
 	 */
-	protected synchronized void addNewSubsetQueue(final Set<Set<ZoneVertex>> newSubsets) {
-		assert newSubsets != null;
-		this.subsets.add(newSubsets);
-		this.centralQueue.addAll(newSubsets);
+	protected GreedyBup(final Set<ZoneVertex> initialSet, final int maximalSubsetSize, final ZoneVertex root, final Comparator<Set<ZoneVertex>> ssComparator) {
+		super(initialSet, maximalSubsetSize, root);
+		
+		//The central queue holds all generated but not yet consumed subsets ordered in their size
+		this.centralQueue = new PriorityQueue<>(ssComparator);
+		
+		if (phase <= lastPhase()) {
+			assert this.queues.size() > 0;
+			
+			this.centralQueue.addAll(this.queues.get(this.phase));
+		}
+	}
+	
+	/**
+	 * Return whether a subset with size {@code p} can be enqueued or not. Extensions of this class may want to overwrite this method for optimization
+	 * @param p			The size of the subset to be created and added to the queues
+	 * @return			Always true
+	 */
+	protected boolean canEnqueue(final int p) {
+		return true;
 	}
 
 	@Override
@@ -137,17 +120,20 @@ class GreedyBup extends Bup {
 			//Create new subsets for the newly added zone vertex from the phase 1 until the last phase
 			int p = -1;
 			Set<Set<ZoneVertex>> newSubsets;
+			Set<Set<ZoneVertex>> queueSubsets;
 			do {
 				p++;
 				newSubsets = createNewSubsets(p, zoneVertex);
+				queueSubsets = canEnqueue(p) ? newSubsets : new HashSet<>(0);
 				
 				//if new subsets were created, add them
 				if (!newSubsets.isEmpty()) {
 					if (this.subsets.size() <= p) {
-						addNewSubsetQueue(newSubsets);
+						this.subsets.add(newSubsets);
+						this.centralQueue.addAll(queueSubsets);
 					} else {
 						this.subsets.get(p).addAll(newSubsets);
-						this.centralQueue.addAll(newSubsets);
+						this.centralQueue.addAll(queueSubsets);
 					}
 				}
 			} while((p + 1) <= lastPhase() && !newSubsets.isEmpty());
@@ -181,17 +167,20 @@ class GreedyBup extends Bup {
 			int p = Math.max(this.phase - 1, 0);
 			if ((p + 1) <= lastPhase()) {
 				Set<Set<ZoneVertex>> newSubsets;
+				Set<Set<ZoneVertex>> queueSubsets;
 				do {
 					p++;
 					newSubsets = createNewSubsets(p, this.bupSet);
+					queueSubsets = canEnqueue(p) ? newSubsets : new HashSet<>(0);
 					
 					if (this.subsets.size() <= p) {
-						addNewSubsetQueue(newSubsets);
+						this.subsets.add(newSubsets);
+						this.centralQueue.addAll(queueSubsets);
 					} else {
 						this.subsets.get(p).addAll(newSubsets);
-						this.centralQueue.addAll(newSubsets);
+						this.centralQueue.addAll(queueSubsets);
 					}
-				} while ((p + 1) <= lastPhase() && newSubsets.isEmpty());
+				} while ((p + 1) <= lastPhase() && queueSubsets.isEmpty());
 				
 				this.phase = p;
 				
