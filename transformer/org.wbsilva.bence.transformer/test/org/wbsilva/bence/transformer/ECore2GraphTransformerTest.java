5b8ae1d826bb2d77e23440109bc0d5f6c60bb306
William Bombardelli da Silva <william.bombardellidasilva@campus.tu-berlin.de>
2018-10-21 13:34:13 +0200

    Add unit test for transformation of non connected graphs

--- transformer/org.wbsilva.bence.transformer/test/org/wbsilva/bence/transformer/ECore2GraphTransformerTest.java
+++ transformer/org.wbsilva.bence.transformer/test/org/wbsilva/bence/transformer/ECore2GraphTransformerTest.java
@@ -2,6 +2,7 @@ package org.wbsilva.bence.transformer;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -24,6 +25,7 @@ class ECore2GraphTransformerTest {
 	private static EObject obj2;
 	private static EObject obj3;
 	private static EObject obj4;
+	private static EObject obj5;
 
 	@SuppressWarnings("unchecked")
 	@BeforeAll
@@ -66,6 +68,8 @@ class ECore2GraphTransformerTest {
 		obj4 = pkg.getEFactoryInstance().create(clazzA);
 		((List<EObject>)obj4.eGet(refB)).add(obj3);
 		((List<EObject>)obj4.eGet(refB)).add(obj4);
+		
+		obj5 = pkg.getEFactoryInstance().create(clazzA);
 	}
 	
 	
@@ -135,6 +139,31 @@ class ECore2GraphTransformerTest {
 		assertEquals(5, g0.getEdges().size());
 		assertTrue(g0.getEdges().stream()
 				.allMatch(e -> e.getLabel().getName().equals("B") || e.getLabel().getName().equals("C")));
+		
+		assertTrue(d.entrySet().stream()
+					.noneMatch(e -> !g0.getVertices().stream()
+							.map(v -> v.getId())
+							.collect(Collectors.toSet())
+							.contains(e.getKey())));
+	}
+	
+	@Test
+	void testTransformNonconnectedGraph() {
+		List<EObject> components = new ArrayList<>();
+		components.add(obj1);
+		components.add(obj5);
+		
+		ECore2GraphTransformer transformer = new ECore2GraphTransformer();
+		E2GTransformationResult r = transformer.transform(components);
+		
+		Graph g0 = r.getGraph();
+		Map<String, Integer> d = r.getDepths();
+		
+		assertTrue(GraphgrammarUtil.isValidGraph(g0, true));
+		assertEquals(2, g0.getVertices().size());
+		assertTrue(g0.getVertices().stream().allMatch(v -> v.getLabel().getName().equals("A")));
+		
+		assertEquals(0, g0.getEdges().size());
 		
 		assertTrue(d.entrySet().stream()
 					.noneMatch(e -> !g0.getVertices().stream()
